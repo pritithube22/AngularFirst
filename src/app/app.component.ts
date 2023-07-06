3b7f06a8e8fb8eeea86c55cb07d85a21df39c83c
pritithube22 <thubepriti197@.com>
2023-07-06 15:06:51 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<h1> First Program : Hello World..!</h1>'
+  
+})
+export class AppComponent {
+  title = 'AngularFirst';
+}
